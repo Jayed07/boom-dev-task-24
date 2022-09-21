50afbbec86a6dfd8131a4384e50824ce0b4d0f65
Jayed07 <jayed.ws7@gmail.com>
2022-09-21 18:55:42 +0300

    pokemon list items added

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,29 @@
+import { data } from "autoprefixer";
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+
+  let pokemonList = [];
+
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+  .then((response) => response.json())
+  .then((data) => {
+
+    data.results.forEach((key) => {
+      pokemonList.push(key.name);
+    });
+
+    console.log(pokemonList)
+
+    pokemonList.forEach((p) => {
+      let li = document.createElement('li');
+      li.innerHTML = p;
+      console.log(li);
+      ul.appendChild(li);
+    })
+  });
+
 });
